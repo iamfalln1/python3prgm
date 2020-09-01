bdab882a7dc978fc8ec6f7b62e3de174180d636b
iamfalln1 <jishnu.s.haridas@gmail.com>
2020-09-01 23:06:35 +0530

    editted

--- printing_with_python.py
+++ printing_with_python.py
@@ -1,4 +1,4 @@
-'''										Printing with Python 3
+'''					 Printing with Python 3
 The print() function prints the given object to the standard output device (screen) or to the text stream file.
 	Python print() syntax ==> print(*objects, sep=' ', end='\n', file=sys.stdout, flush=False)    '''
 print(10);# printing a number
@@ -6,6 +6,7 @@ print("I LOVE PYTHON");# Printing a string
 print("Hello World");
 print('This is a test file for python programming. It prints out the content inside the parenthesis');
 print('python is Fun');
+# multiple objects can be passed in print()
 a=5;
 print('a = ', a); # 2 objects are passed in print() function
 b=a;
