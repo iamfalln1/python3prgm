4f0f455238132048e56526daf56aa432158b2d53
iamfalln1 <jishnu.s.haridas@gmail.com>
2020-09-10 03:23:53 +0530

    aesthetic improvement

--- python_def_function.py
+++ python_def_function.py
@@ -4,14 +4,14 @@ def hello():
 	print('Hello World, This is Python 3 Programming'); # atleast one whitespce is needed 
 
 hello();
-
+print("Calculator Function:")
 # implementation of function in a program
-x = int(input()); # int(input()) must be used because input default to str without defining it
-y = int(input());
+x = int(input("Enter a Number:- ")); # int(input()) must be used because input default to str without defining it
+y = int(input("Enter another Number:- "));
 def cal(): # declaring a function 
-	print('Sum is ', x + y)
-	print('Difference is ', x - y)
-	print('Product is ', x * y)
-	print('Qoutient is ', x / y, 'and Reminder is ', x % y)
+	print('Sum is ',x + y)
+	print('Difference is ',x - y)
+	print('Product is ',x * y)
+	print('Qoutient is ',x / y, 'and Reminder is ',x % y)
 
 cal(); # calling the function for running	
